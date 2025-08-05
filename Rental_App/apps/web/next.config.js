890faab266f60473f1b33f34c796e2d2535231dc
Billy Rochester <honesthomesales@gmail.com>
2025-08-04 21:18:15 -0400

    Update deployment configuration for Vercel

--- Rental_App/apps/web/next.config.js
+++ Rental_App/apps/web/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: 'https://gnisgfojzrrnidizrycj.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduaXNnZm9qenJybmlkaXpyeWNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjgyMDMsImV4cCI6MjA2NzM0NDIwM30.jLRIt4mqNa-6rnWudT_ciCvfPC0i0WlWFrCgC7NbhYM',
   },
-  output: 'export',
-  trailingSlash: true,
+  output: 'standalone',
+  trailingSlash: false,
   generateBuildId: async () => {
     return 'build'
   },
